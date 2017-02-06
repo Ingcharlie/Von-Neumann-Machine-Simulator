3e3dba9d3331d995c127cf4074c499c826354b09
Ingcharlie <cacardenas7@hotmail.com>
2017-02-06 10:19:19 -0500

    Add files via upload

---
+++ src/simulator/von/neumann/SimulatorVonNeumann.java
@@ -0,0 +1,16 @@
+package simulator.von.neumann;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @author cacar
+ */
+public class SimulatorVonNeumann {
+
+    public void graficar(Graphics g, int x, int y, int x1, int y1){
+        
+        g.drawLine(x, y, x1, y1);
+    }
+    
+}
